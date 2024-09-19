8a8953434e6ad1ddd42c6a8df161106c6f56092d
TheDoctorsEquivalence909er <chenrichs0822@gmail.com>
2024-09-19 18:43:21 -0500

    add comments

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
 import ollama
 
+# function for asking the AI
 def askAI(question):
+    # preText to give better respones 
     preText = "Give concise answers to following questions and add \n if line goes over 80 charters long"
+    # Asking ollama and Specifying parameters
     stream = ollama.chat(
         model='phi3',
         messages=[{'role': 'user', 'content':preText+question}],
         stream=True,
     )
-
+    # stream of AI output 
     return stream
 
+# open input file and stores the text into file_content
 with open('input.txt', 'r', encoding='utf-8', errors="ignore") as file:
     file_content = file.read()
 
-print(file_content)
+# bose the questions to the AI 
 stream = askAI(file_content)
 
+# openes the file for the output and write to it 
 text_file = open("Output.txt", "w", encoding="utf-8", errors="ignore")
+# as the AI is generating the answer it wirte it to the text file 
 for chunk in stream:
     text_file.write(chunk['message']['content'])
     text_file.flush()  # Ensure the content is written immediately
